686db34407fb90a91e93f57e7ec57295a55bdf06
Manvendra <manvendra.kumar7@gmail.com>
2025-04-09 16:01:30 +0530

    using default values fn in db

--- src/drizzle/schemaHelpers.ts
+++ src/drizzle/schemaHelpers.ts
@@ -2,13 +2,16 @@ import { text } from 'drizzle-orm/sqlite-core';
 import { DateTime } from 'luxon';
 import { v7 } from 'uuid';
 
-export const id = text('id').notNull().primaryKey().default(v7());
+export const id = text('id')
+	.notNull()
+	.primaryKey()
+	.$defaultFn(() => v7());
 
 export const createdAt = text('created_at')
 	.notNull()
-	.default(DateTime.now().toISO());
+	.$defaultFn(() => DateTime.now().toISO());
 
 export const updatedAt = text('updated_at')
 	.notNull()
-	.default(DateTime.now().toISO())
+	.$defaultFn(() => DateTime.now().toISO())
 	.$onUpdate(() => DateTime.now().toISO());
