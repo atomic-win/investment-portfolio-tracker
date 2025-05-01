2a21672fef10c5b4bb765dc69588bb7020198cab
Manvendra <manvendra.kumar7@gmail.com>
2025-05-02 01:28:20 +0530

    fix: update label function to display asset class and type in Portfolio sections

--- src/features/portfolio/components/PortfolioPerAssetClassSection.tsx
+++ src/features/portfolio/components/PortfolioPerAssetClassSection.tsx
@@ -61,7 +61,9 @@ export default function PortfolioPerAssetClassSection({
 		<div className='mx-auto'>
 			<PortfolioCharts
 				portfolios={portfolios}
-				labelFn={(portfolio) => portfolio.id}
+				labelFn={(portfolio) =>
+					displayAssetClassText(portfolio.id as AssetClass)
+				}
 			/>
 			<DataTable
 				columns={columns}
