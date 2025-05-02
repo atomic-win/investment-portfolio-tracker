536983b42fe77b69f685558e0790235f8a94ff29
Manvendra <manvendra.kumar7@gmail.com>
2025-05-02 13:44:05 +0530

    Settings API integration (#15)

---
+++ src/hooks/useUpdateSettingsMutation.ts
@@ -0,0 +1,30 @@
+'use client';
+
+import { UserSettingTable } from '@/drizzle/schema';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { usePrimalApiClient } from '@/hooks/usePrimalApiClient';
+
+export default function useUpdateSettingsMutation() {
+	const queryClient = useQueryClient();
+	const primalApiClient = usePrimalApiClient();
+
+	return useMutation({
+		mutationFn: async (
+			settings: Partial<
+				Omit<
+					typeof UserSettingTable.$inferSelect,
+					'id' | 'createdAt' | 'updatedAt'
+				>
+			>
+		) => {
+			await primalApiClient.put('users/me/settings', {
+				...settings,
+			});
+		},
+		async onSuccess() {
+			await queryClient.invalidateQueries({
+				queryKey: ['users', 'me', 'settings'],
+			});
+		},
+	});
+}
