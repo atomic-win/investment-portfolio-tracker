fb79ab97c0fa3e40899be900855ef3f3482367d7
Manvendra <manvendra.kumar7@gmail.com>
2025-06-05 18:33:39 +0530

    Refactor Asset Items page layout with header and add button

--- src/app/assetitems/page.tsx
+++ src/app/assetitems/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 import SidebarTriggerWithBreadcrumb from '@/components/SidebarTriggerWithBreadcrumb';
-import { Card, CardContent } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import AssetItemsTable from '@/features/assetItems/components/AssetItemsTable';
 import { withAssetItemPortfolios } from '@/features/portfolio/hoc/withAssetItemPortfolios';
 import withAssetItems from '@/features/assetItems/hoc/withAssetItems';
 import withCurrency from '@/components/hoc/withCurrency';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { PlusIcon } from 'lucide-react';
 
 export default function Page() {
 	const WithLoadedAssetItemsTable = withAssetItems(
@@ -18,13 +21,18 @@ export default function Page() {
 				breadcrumbs={[{ title: 'Asset Items', href: '/assetitems' }]}
 			/>
 			<div className='container mx-auto p-2'>
-				<Card className='mx-auto rounded-lg shadow-md w-full p-2'>
-					<CardContent className='p-6'>
-						<WithLoadedAssetItemsTable
-							assetItemIds={[]}
-							latest={true}
-							transactions={[]}
-						/>
+				<Card className='mx-auto rounded-lg shadow-md w-full p-2 gap-4'>
+					<CardHeader className='border-b'>
+						<CardTitle className='text-3xl'>Asset Items</CardTitle>
+					</CardHeader>
+					<CardContent className='px-6 pb-6'>
+						<div className='flex justify-end text-xl font-semibold items-center mb-2'>
+							<Button>
+								<PlusIcon />
+								<Link href={`/assetitems/add`}>Add Asset Item</Link>
+							</Button>
+						</div>
+						<WithLoadedAssetItemsTable assetItemIds={[]} latest={true} />
 					</CardContent>
 				</Card>
 			</div>
