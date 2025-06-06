8a139928abae2f0b06f208090690e6204c315cc6
Manvendra <manvendra.kumar7@gmail.com>
2025-06-06 21:14:15 +0530

    Refactor asset item pages to use Suspense for loading state management

--- src/app/assetitems/[assetItemId]/transactions/add/page.tsx
+++ src/app/assetitems/[assetItemId]/transactions/add/page.tsx
@@ -1,6 +1,21 @@
+import LoadingComponent from '@/components/LoadingComponent';
 import AddTransactionPage from '@/features/transactions/components/AddTransactionPage';
+import { Suspense } from 'react';
 
-export default async function Page({
+export default function Page({
+	params,
+}: {
+	params: Promise<{ assetItemId: string }>;
+}) {
+	return (
+		<Suspense
+			fallback={<LoadingComponent loadingMessage='Loading asset item...' />}>
+			<SuspendedPage params={params} />
+		</Suspense>
+	);
+}
+
+async function SuspendedPage({
 	params,
 }: {
 	params: Promise<{ assetItemId: string }>;
