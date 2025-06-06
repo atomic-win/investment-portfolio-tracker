0a149e1f63e0f5295dbfb471942b112ee226d8fc
Manvendra <manvendra.kumar7@gmail.com>
2025-06-06 21:16:23 +0530

    Fixing Suspense build errors (#49)

--- src/app/assetitems/[assetItemId]/@modal/(.)transactions/add/page.tsx
+++ src/app/assetitems/[assetItemId]/@modal/(.)transactions/add/page.tsx
@@ -1,6 +1,21 @@
+import LoadingComponent from '@/components/LoadingComponent';
 import AddTransactionModal from '@/features/transactions/components/AddTransactionModal';
+import { Suspense } from 'react';
 
-export default async function Page({
+export default function Page({
+	params,
+}: {
+	params: Promise<{ assetItemId: string }>;
+}) {
+	return (
+		<Suspense
+			fallback={<LoadingComponent loadingMessage='Loading asset item...' />}>
+			<SuspendedPage params={params} />
+		</Suspense>
+	);
+}
+
+async function SuspendedPage({
 	params,
 }: {
 	params: Promise<{ assetItemId: string }>;
--- src/app/assetitems/[assetItemId]/page.tsx
+++ src/app/assetitems/[assetItemId]/page.tsx
@@ -1,10 +1,26 @@
+import LoadingComponent from '@/components/LoadingComponent';
 import AssetItemPage from '@/features/assetItems/components/AssetItemPage';
+import { Suspense } from 'react';
 
-export default async function Page({
+export default function Page({
+	params,
+}: {
+	params: Promise<{ assetItemId: string }>;
+}) {
+	return (
+		<Suspense
+			fallback={<LoadingComponent loadingMessage='Loading asset item...' />}>
+			<SuspendedPage params={params} />
+		</Suspense>
+	);
+}
+
+async function SuspendedPage({
 	params,
 }: {
 	params: Promise<{ assetItemId: string }>;
 }) {
 	const { assetItemId } = await params;
+
 	return <AssetItemPage assetItemId={assetItemId} />;
 }
