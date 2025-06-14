6bf90472605db38c82c6ffdfddcf525ec599dbdf
Manvendra <manvendra.kumar7@gmail.com>
2025-06-14 20:03:58 +0530

    chore: update package.json with new linting and formatting scripts, add eslint-config, and update eslint version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,34 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import stylistic from '@stylistic/eslint-plugin';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default defineConfig([
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	{
+		plugins: {
+			'@stylistic': stylistic,
+		},
+		rules: {
+			'@stylistic/indent': ['error', 'tab'],
+			'@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+			'@stylistic/semi': ['error', 'always'],
+		},
+	},
+	globalIgnores([
+		'node_modules',
+		'dist',
+		'build',
+		'coverage',
+		'public',
+		'.next',
+	]),
+]);
