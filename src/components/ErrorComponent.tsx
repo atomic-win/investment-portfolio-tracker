d8c879502f0879ca449e3cd1d9369a627635887b
Manvendra <manvendra.kumar7@gmail.com>
2025-10-18 21:47:10 +0530

    Improving UI of Loading and Error components (#70)

--- src/components/ErrorComponent.tsx
+++ src/components/ErrorComponent.tsx
@@ -1,3 +1,6 @@
+import { TriangleAlertIcon } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 
 export default function ErrorComponent({
@@ -7,7 +10,10 @@ export default function ErrorComponent({
 }) {
 	return (
 		<div className='flex items-center justify-center w-full h-full'>
-			<Label className='text-destructive'>{errorMessage}</Label>
+			<Button size='sm' variant='ghost'>
+				<TriangleAlertIcon color='red' />
+				<Label className='text-destructive'>{errorMessage}</Label>
+			</Button>
 		</div>
 	);
 }
