3d0632c340d8aa2d5b4714d17fa61e69cebe7414
Manvendra <manvendra.kumar7@gmail.com>
2025-12-14 13:55:23 +0530

    Remove unused transaction and user-related database functions and cache tags

--- src/features/transactions/server/cacheTag.ts
+++
@@ -1,7 +0,0 @@
-export function transactionsTag(assetItemId: string) {
-	return `assetItems/${assetItemId}/transactions/all`;
-}
-
-export function transactionTag(assetItemId: string, id: string) {
-	return `assetItems/${assetItemId}/transactions/${id}`;
-}
--- src/features/users/cacheTag.ts
+++
@@ -1,7 +0,0 @@
-export function getUserTag(userId: string) {
-	return `users/${userId}`;
-}
-
-export function getUserSettingsTag(userId: string) {
-	return `${getUserTag(userId)}/settings`;
-}
