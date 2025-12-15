9bbf5cfc2a615590f275b13871b36978d881ad6f
Manvendra <manvendra.kumar7@gmail.com>
2025-12-16 00:23:22 +0530

    refactor: update Breadcrumb components to use new base UI library and improve accessibility

--- src/components/SidebarTriggerWithBreadcrumb.tsx
+++ src/components/SidebarTriggerWithBreadcrumb.tsx
@@ -22,14 +22,17 @@ export default function SidebarTriggerWithBreadcrumb({
 }) {
 	breadcrumbs = [{ title: 'Home', href: '/' }, ...breadcrumbs];
 	return (
-		<header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
+		<header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
 			<div className='flex items-center gap-2 px-4'>
 				<SidebarTrigger className='-ml-1' />
 				<Separator orientation='vertical' className='mr-2 h-4' />
 				<Breadcrumb>
 					<BreadcrumbList>
 						{breadcrumbs.map(({ title, href, disabled }, index) => (
-							<div key={index} className='flex items-center gap-2'>
+							<div
+								key={index}
+								className='flex items-center gap-2'
+							>
 								{index > 0 && (
 									<BreadcrumbSeparator className='block' />
 								)}
@@ -37,11 +40,15 @@ export default function SidebarTriggerWithBreadcrumb({
 									{index === breadcrumbs.length - 1 ? (
 										<BreadcrumbPage>{title}</BreadcrumbPage>
 									) : disabled ? (
-										<BreadcrumbLink asChild>
-											<span className='text-muted-foreground'>{title}</span>
+										<BreadcrumbLink>
+											<span className='text-muted-foreground'>
+												{title}
+											</span>
 										</BreadcrumbLink>
 									) : (
-										<BreadcrumbLink href={href}>{title}</BreadcrumbLink>
+										<BreadcrumbLink href={href}>
+											{title}
+										</BreadcrumbLink>
 									)}
 								</BreadcrumbItem>
 							</div>
