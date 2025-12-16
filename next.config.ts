e8a31ce29c297ceffc0145099f53ac3c947ec44e
Manvendra <manvendra.kumar7@gmail.com>
2025-12-17 04:15:28 +0530

    Fix monthly trends dates (#82)

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-	/* config options here */
-	cacheComponents: true,
-	cacheLife: {
-		daily: {
-			stale: 60 * 60 * 24, // 1 day
-			revalidate: 60 * 60, // 1 hour
-			expire: 60 * 60 * 24, // 1 day
-		},
-		expireNow: {
-			stale: 0,
-			revalidate: 0,
-			expire: 0,
-		},
-	},
-};
+const nextConfig: NextConfig = {};
 
 module.exports = nextConfig;
