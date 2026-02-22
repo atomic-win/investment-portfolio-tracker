1c71f2b4fb5707b8a2e28771f47ec663441cc264
Manvendra <manvendra.kumar7@gmail.com>
2026-02-22 19:12:29 +0530

    Fix package vulnerabilities (#104)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,76 +1,18 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-import { FlatCompat } from '@eslint/eslintrc';
-import stylistic from '@stylistic/eslint-plugin';
 import { defineConfig, globalIgnores } from 'eslint/config';
-import importPlugin from 'eslint-plugin-import';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-});
-
-export default defineConfig([
-	...compat.extends('next/core-web-vitals', 'next/typescript'),
-	{
-		plugins: {
-			'@stylistic': stylistic,
-			import: importPlugin,
-		},
-		rules: {
-			'@stylistic/indent': ['error', 'tab'],
-			'@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
-			'@stylistic/semi': ['error', 'always'],
-			'no-restricted-imports': [
-				'error',
-				{
-					patterns: ['./*', '../*'],
-				},
-			],
-			'import/order': [
-				'error',
-				{
-					groups: [
-						'builtin', // Node.js builtins like fs, path
-						'external', // Packages from node_modules
-						'internal', // Your alias paths (e.g. "@/utils")
-						['parent', 'sibling', 'index'],
-						'object',
-						'type',
-					],
-					pathGroups: [
-						{
-							pattern: '@/**',
-							group: 'internal',
-							position: 'after',
-						},
-					],
-					pathGroupsExcludedImportTypes: ['builtin'],
-					alphabetize: {
-						order: 'asc',
-						caseInsensitive: true,
-					},
-					'newlines-between': 'always',
-				},
-			],
-		},
-		settings: {
-			'import/resolver': {
-				typescript: {
-					project: './tsconfig.json',
-				},
-			},
-		},
-	},
+const eslintConfig = defineConfig([
+	...nextVitals,
+	...nextTs,
+	// Override default ignores of eslint-config-next.
 	globalIgnores([
-		'node_modules',
-		'dist',
-		'build',
-		'coverage',
-		'public',
-		'.next',
+		// Default ignores of eslint-config-next:
+		'.next/**',
+		'out/**',
+		'build/**',
+		'next-env.d.ts',
 	]),
 ]);
+
+export default eslintConfig;
