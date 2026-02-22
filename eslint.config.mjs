fbd5cc8ff0da000c19871b01c4d5f17d798ce2c8
Manvendra <manvendra.kumar7@gmail.com>
2026-02-22 17:57:21 +0530

    Update ESLint configuration and bump package versions to address vulnerabilities

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,12 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-import { FlatCompat } from '@eslint/eslintrc';
 import stylistic from '@stylistic/eslint-plugin';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 import importPlugin from 'eslint-plugin-import';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-});
-
 export default defineConfig([
-	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	...nextVitals,
+	...nextTs,
 	{
 		plugins: {
 			'@stylistic': stylistic,
