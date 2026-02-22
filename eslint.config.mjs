6bb12018b56bf713a19b8def4ab29749fad50327
Manvendra <manvendra.kumar7@gmail.com>
2026-02-22 18:44:04 +0530

    chore: remove unused ESLint plugins to address vulnerabilities

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,68 +1,18 @@
-import stylistic from '@stylistic/eslint-plugin';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
-import importPlugin from 'eslint-plugin-import';
 
-export default defineConfig([
+const eslintConfig = defineConfig([
 	...nextVitals,
 	...nextTs,
-	{
-		plugins: {
-			'@stylistic': stylistic,
-			import: importPlugin,
-		},
-		rules: {
-			'@stylistic/indent': ['error', 'tab'],
-			'@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
-			'@stylistic/semi': ['error', 'always'],
-			'no-restricted-imports': [
-				'error',
-				{
-					patterns: ['./*', '../*'],
-				},
-			],
-			'import/order': [
-				'error',
-				{
-					groups: [
-						'builtin', // Node.js builtins like fs, path
-						'external', // Packages from node_modules
-						'internal', // Your alias paths (e.g. "@/utils")
-						['parent', 'sibling', 'index'],
-						'object',
-						'type',
-					],
-					pathGroups: [
-						{
-							pattern: '@/**',
-							group: 'internal',
-							position: 'after',
-						},
-					],
-					pathGroupsExcludedImportTypes: ['builtin'],
-					alphabetize: {
-						order: 'asc',
-						caseInsensitive: true,
-					},
-					'newlines-between': 'always',
-				},
-			],
-		},
-		settings: {
-			'import/resolver': {
-				typescript: {
-					project: './tsconfig.json',
-				},
-			},
-		},
-	},
+	// Override default ignores of eslint-config-next.
 	globalIgnores([
-		'node_modules',
-		'dist',
-		'build',
-		'coverage',
-		'public',
-		'.next',
+		// Default ignores of eslint-config-next:
+		'.next/**',
+		'out/**',
+		'build/**',
+		'next-env.d.ts',
 	]),
 ]);
+
+export default eslintConfig;
